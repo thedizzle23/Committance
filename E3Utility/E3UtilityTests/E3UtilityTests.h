ac6b5dd95e5ec8ab58e640ac6d6824f4bab63f74
Robert DeNicola <thedizzle23@gmail.com>
2012-05-31 15:19:32 -0400

    damnit

---
+++ E3Utility/E3UtilityTests/E3UtilityTests.h
@@ -0,0 +1,13 @@
+//
+//  E3UtilityTests.h
+//  E3UtilityTests
+//
+//  Created by Robert DeNicola on 5/31/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface E3UtilityTests : SenTestCase
+
+@end
---
+++ E3Utility/E3UtilityTests/E3UtilityTests.m
@@ -0,0 +1,32 @@
+//
+//  E3UtilityTests.m
+//  E3UtilityTests
+//
+//  Created by Robert DeNicola on 5/31/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "E3UtilityTests.h"
+
+@implementation E3UtilityTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in E3UtilityTests");
+}
+
+@end
