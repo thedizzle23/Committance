02512fd587126edd1cb597665e1147405c17b358
Robert DeNicola <thedizzle23@gmail.com>
2012-05-31 14:30:31 -0400

    changed settings

---
+++ FineAndDandyCash/FineAndDandyCashTests/FineAndDandyCashTests.h
@@ -0,0 +1,13 @@
+//
+//  FineAndDandyCashTests.h
+//  FineAndDandyCashTests
+//
+//  Created by Robert DeNicola on 5/31/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FineAndDandyCashTests : SenTestCase
+
+@end
---
+++ FineAndDandyCash/FineAndDandyCashTests/FineAndDandyCashTests.m
@@ -0,0 +1,32 @@
+//
+//  FineAndDandyCashTests.m
+//  FineAndDandyCashTests
+//
+//  Created by Robert DeNicola on 5/31/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "FineAndDandyCashTests.h"
+
+@implementation FineAndDandyCashTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FineAndDandyCashTests");
+}
+
+@end
